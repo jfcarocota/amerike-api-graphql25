bb68cb21b7d4234ab4707536990088e467262100
jcaro <jcaro@amberstudio.com>
2025-03-22 10:24:51 -0700

    return character data list

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,6 +1,8 @@
+import CharacterData from './models/characterData.js'
+
 export const resolvers = {
   Query: {
     Hello: (root, args) => "Hello World, Graphql",
-    //CharacterDataList: async (root, args) => 
+    CharacterDataList: async (root, args) => CharacterData.find({}), //fin all
   }
 }
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,5 +1,12 @@
 export const typeDefs = `#graphql 
   type Query{
     Hello: String!
+    CharacterDataList: [CharacterData]!
+  },
+  type CharacterData{
+    id: ID!,
+    moveSpeed: Float!
+    jumpForce: Float!
+    styleName: String!
   }
 `;
