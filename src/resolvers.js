62ffd19be28e16df15eb9340367e5988d2aca548
jcaro <jcaro@amberstudio.com>
2025-03-22 10:39:58 -0700

    get character data by style name

--- src/resolvers.js
+++ src/resolvers.js
@@ -4,5 +4,9 @@ export const resolvers = {
   Query: {
     Hello: (root, args) => "Hello World, Graphql",
     CharacterDataList: async (root, args) => CharacterData.find({}), //fin all
+    CharacterDataByStyleName: async (root, args) => {
+      const {styleName} = args;
+      return CharacterData.find({styleName});
+    },
   }
 }
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -2,6 +2,7 @@ export const typeDefs = `#graphql
   type Query{
     Hello: String!
     CharacterDataList: [CharacterData]!
+    CharacterDataByStyleName(styleName: String!): [CharacterData]!
   },
   type CharacterData{
     id: ID!,
