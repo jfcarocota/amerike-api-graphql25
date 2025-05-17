7b20e89779fad7b7ac8c6561585eb745a6fadb88
Jesús Caro <jfcarocota@gmail.com>
2025-05-17 15:36:46 -0700

    remove useless stuffs

--- src/schemas/Schema.js
+++
@@ -1,8 +0,0 @@
-import graphql, { GraphQLSchema } from 'graphql';
-import Query from './Query.js';
-import Mutation from './Mutation.js';
-
-export default new GraphQLSchema({
-  query: Query,
-  mutation: Mutation,
-})
--- src/server.js
+++ src/server.js
@@ -1,15 +1,9 @@
 import { ApolloServer } from "@apollo/server";
 import {typeDefs} from './typeDefs.js';
 import {resolvers} from './resolvers.js';
-import Schema from "./schemas/Schema.js";
 
 export const server = new ApolloServer({
   typeDefs,
   resolvers,
   csrfPrevention: false
 });
-
-/*export const server = new ApolloServer({
-  schema: Schema,
-  csrfPrevention: false
-});*/
